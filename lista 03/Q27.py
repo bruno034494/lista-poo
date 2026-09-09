8fb67d193ae7525aff29b4edccb22e962f1c1024
bruno034494 <brunodesouzabueno4@gmail.com>
2026-09-08 23:22:43 -0300

    Remove unnecessary file writing code from Q27.py

--- lista 03/Q27.py
+++
@@ -1,4 +0,0 @@
-with open("arquivo.txt", "w") as arquivo:
-    arquivo.write("Olá, mundo!\n")
-    arquivo.write("Este é um arquivo de teste.\n")
-    arquivo.write("Escrevendo várias linhas de texto.\n")
